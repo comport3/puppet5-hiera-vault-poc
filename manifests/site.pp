295ee9efffc389c85db7acb5e09cfaea9beaeed4
Grant Davies <grant@Grants-MacBook-Pro.local>
2018-07-07 10:03:30 +1000

    .

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,9 +1,3 @@
 node default {
   include profile::vault
-#  file { '/root/demo':
-#    ensure  => file,
-#    content => inline_epp(password=lookup('vault_notify')),
-##   content => inline_epp($ntp_conf_template, {'service_name' => 'xntpd', 'iburst_enable' => true}),
-## notify { "vault parameter: ${lookup({"name" => "vault_notify", "default_value" => "Failed to lookup value."})}":}
-#  }
 }
